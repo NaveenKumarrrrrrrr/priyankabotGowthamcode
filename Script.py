f0cb3caed02d5323f0739a6363a8e049114a7d0a
NaveenKumarrrrrrrr <naveenramasamy04@gmail.com>
2024-06-09 14:05:44 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -84,7 +84,7 @@ class script(object):
 ›› /set_template - 𝚂𝙴𝚃 𝙲𝚄𝚂𝚃𝙾𝙼 𝙸𝙼𝙳𝙱 𝚃𝙴𝙼𝙿𝙻𝙰𝚃𝙴 𝙵𝙾𝚁 𝙰𝚄𝚃𝙾 𝙵𝙸𝙻𝚃𝙴𝚁. 
 ›› /get_template - 𝙶𝙴𝚃 𝙲𝚄𝚁𝚁𝙴𝙽𝚃 𝙸𝙼𝙳𝙱 𝚃𝙴𝙼𝙿𝙻𝙰𝚃𝙴 𝙾𝙵 𝙰𝚄𝚃𝙾 𝙵𝙸𝙻𝚃𝙴𝚁.
 
-𝙲𝚁𝙴𝙳𝙸𝚃𝚂 :- <a href=https://t.me/GM_Botzz> Gᴍ Bᴏᴛᴢᴢ™</a>"""
+𝙲𝚁𝙴𝙳𝙸𝚃𝚂 :- <a href=https://t.me/movies_tamizhaass> Movies Tamizhaass™</a>"""
 
     CONNECTION_TXT = """Help: <b>Connections</b>
 
